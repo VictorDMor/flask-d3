b899a0d24285475f61629a0e9448ebd97cd422cc
Diogo Lundberg <diogo@lundberg.com.br>
2016-02-22 16:25:41 -0300

    Refactors treemap js file to a more 'class' oriented structure

--- app/static/js/treemap.js
+++ app/static/js/treemap.js
@@ -1,49 +1,53 @@
 var treemap = (function() {
-  return {
-    init: init
-  }
-
-  function init(){
-    var treemap = {
-      _name: "tree_map",
-      _items: [],
-      _id: "hs_id",
-      _size: "export_val",
+    return {
+        draw: draw,
+        init: init,
     }
 
-    function graph_data(converted_data) {
-      d3.json("static/flare.json", function(error, root) {
+    var _name,
+        _id,
+        _size;
+
+    function _getData() {
+        var  data = []
+        d3.json("static/flare.json", function(error, root) {
         if (error) throw error;
           for (i = 0; i < root.data.length; i++) {
-            converted_data[i] = {
+            data[i] = {
               "year": root.data[i][0],
               "import_val": root.data[i][2],
               "export_val": root.data[i][3],
-              "hs_id": root.data[i][root.data.length] 
+              "hs_id": root.data[i][root.data.length]
             }
           }
-      });
-      return converted_data
+        });
+
+        return data
     }
 
-    function graph_retrieval(graph_type) {
-      var visualization = d3plus.viz()
-      .container("#viz")
-      .data(graph_data([]))
-      .type(treemap._name)
-      .id(treemap._id)
-      .depth(1)
-      .size(treemap._size)
-      .labels({"align": "left", "valign": "top"})
-      .draw();
+    function draw() {
+        var visualization = d3plus.viz()
+            .container("#viz")
+            .data(_getData())
+            .type(_name)
+            .id(_id)
+            .depth(1)
+            .size(_size)
+            .labels({"align": "left", "valign": "top"})
+            .draw();
     }
 
-    graph_retrieval(treemap)
-  }
+    function init(){
+        _name = "tree_map";
+        _id = "hs_id";
+        _size = "export_val";
+    }
+
 })();
 
 $(document).ready(function() {
-  treemap.init();
+    treemap.init();
+    treemap.draw();
 });
 
 
